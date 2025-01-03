db4f09a166a2a8dcc09f74d6115799f6f3cbde53
aniket kumar <118229208+ankymohi@users.noreply.github.com>
2025-01-03 07:27:19 -0800

    Update server.js

--- api/server.js
+++ api/server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors(
  {
-  origin:["https://getninjasmanagment-emuy.vercel.app/"],
+  origin:["https://getninjasmanagment-emuy.vercel.app"],
   methods:["POST","GET","PUT"],
   credentials:true
  }
